956b6725d09e7144d5119ffd3bf1c4e275954ea6
Pheonix05 <yashd7200@gmail.com>
2024-10-11 14:09:10 +0530

    Sign Up for HR API is created and Tested

--- src/main/java/com/franchiseWorld/EmployeeManagementSystem/LoginRegister/AuthController.java
+++ src/main/java/com/franchiseWorld/EmployeeManagementSystem/LoginRegister/AuthController.java
@@ -87,5 +87,56 @@ public class AuthController {
 
     }
 
+    @PostMapping("/signup/HR")
+    public ResponseEntity<AuthResponse> createHRHandler(@Valid @RequestBody User user) throws UserException {
+
+        String email = user.getEmail();
+        String password = user.getPassword();
+        String firstName=user.getFirstName();
+        String lastName=user.getLastName();
+        UserRole userRole = UserRole.ROLE_HR;
+        String mobile = user.getMobile();
+        List<Address> address = user.getAddresses();
+        Position position = user.getPosition();
+        Department department = user.getDepartment();
+
+        User isEmailExist=userRepository.findByEmail(email);
+
+        // Check if user with the given email already exists
+        if (isEmailExist!=null) {
+            // System.out.println("--------- exist "+isEmailExist).getEmail());
+
+            throw new UserException("Email Is Already Used With Another Account");
+        }
+
+        // Create new user
+        User createdUser= new User();
+        createdUser.setEmail(email);
+        createdUser.setFirstName(firstName);
+        createdUser.setLastName(lastName);
+        createdUser.setPassword(passwordEncoder.encode(password));
+        createdUser.setRole(UserRole.ROLE_HR);
+        createdUser.setMobile(mobile);
+        createdUser.setAddresses(address);
+        createdUser.setPosition(position);
+        createdUser.setDepartment(department);
+
+
+
+        User savedUser= userRepository.save(createdUser);
+
+
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtTokenProvider.generateToken(authentication);
+
+        AuthResponse authResponse= new AuthResponse(token,true);
+
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
+
+    }
+
 }
 
